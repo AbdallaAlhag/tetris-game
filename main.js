f0c21ee9a648767bd9632ba23507aa31aec68299
Abdalla <alhagabdalla@gmail.com>
2025-08-19 08:29:22 -0700

    small changes

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 import { Application, Container, Assets, Sprite } from "pixi.js";
 
+// TODO:
+// add single piece
+//  - piece moving down (left or right)
+//  - ability to rotate (up)
+//  - speed up button (down)
+//  - ability to jump down instantly
+//    - with shadow piece to showcase where it will land
+
 (async () => {
   const app = new Application();
   globalThis.__PIXI_APP__ = app;
@@ -17,18 +25,45 @@ import { Application, Container, Assets, Sprite } from "pixi.js";
   console.log(`Base Url: ${BASEURL}`);
 
   // single block is 64 x 64 pixels,
-  // board is 768 x 1408 pixels
+  // board is 768 x 1408 pixels, which is 12 pieces width and 22 pieces height
+  // but board is also 10 width and 20 high without border
   // let's make a container this time
-  const container = new Container();
-  app.stage.addChild(container);
+  const board = new Container();
+  app.stage.addChild(board);
 
   const backgroundTexture = await Assets.load(BASEURL + "/images/Board.png");
   const backGroundSprite = Sprite.from(backgroundTexture);
   backGroundSprite.scale.set(0.5, 0.5);
-  container.addChild(backGroundSprite);
+  board.addChild(backGroundSprite);
 
-  container.pivot.set(container.width / 2, container.height / 2);
-  container.position.set(app.screen.width / 2, app.screen.height / 2);
+  board.pivot.set(board.width / 2, board.height / 2);
+  board.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  // Top and Bottom x of the board
+  const BOARDPIECEHEIGHT = board.height / 22;
+  const TOPX = 0;
+  const BOTTOMX = board.height - BOARDPIECEHEIGHT * 3; // board.height - 3 board pieces
+  const MIDDLEX = board.width / 2;
 
   // test code for simple piece
+  const piece1Texture = await Assets.load(BASEURL + "/images/I.png");
+  const piece1Sprite = Sprite.from(piece1Texture);
+  piece1Sprite.scale.set(0.5);
+
+  // piece1Sprite.rotation = Math.PI / 4;
+  piece1Sprite.anchor.set(0.5, 0);
+  piece1Sprite.x = board.width / 2;
+  board.addChild(piece1Sprite);
+
+  let dropCounter = 0;
+  const dropInterval = 30; // frames before piece moves down (~0.5s at 60fps)
+  console.log(piece1Sprite);
+  app.ticker.add((delta) => {
+    dropCounter += delta.deltaTime;
+    if (dropCounter >= dropInterval && piece1Sprite.position.y <= BOTTOMX) {
+      piece1Sprite.position.y += BOARDPIECEHEIGHT;
+      dropCounter = 0; // reset counter
+      console.log(piece1Sprite.position.y);
+    }
+  });
 })();
